045ad323dc74510f82d97d451293107890888722
Michal Piechowiak <misiek.piechowiak@gmail.com>
2021-01-31 22:09:02 +0100

    paramless refresh

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,24 +1,24 @@
-const path = require(`path`)
-const fs = require(`fs-extra`)
+const path = require(`path`);
+const fs = require(`fs-extra`);
 
-let runNumber = parseInt(process.env.ARTIFACTS_RUN_SETUP, 10) || 1
+let runNumber = parseInt(process.env.ARTIFACTS_RUN_SETUP, 10) || 1;
+let paramLessSourceRefreshes = 0;
+let isFirstRun = runNumber === 1;
 
-let isFirstRun = runNumber === 1
-
-let changedSsrCompilationHash
-let changedBrowserCompilationHash
+let changedSsrCompilationHash;
+let changedBrowserCompilationHash;
 
 exports.onPreInit = ({ emitter }) => {
-  emitter.on(`SET_SSR_WEBPACK_COMPILATION_HASH`, action => {
-    changedSsrCompilationHash = action.payload
-  })
+  emitter.on(`SET_SSR_WEBPACK_COMPILATION_HASH`, (action) => {
+    changedSsrCompilationHash = action.payload;
+  });
 
-  emitter.on(`SET_WEBPACK_COMPILATION_HASH`, action => {
-    changedBrowserCompilationHash = action.payload
-  })
-}
+  emitter.on(`SET_WEBPACK_COMPILATION_HASH`, (action) => {
+    changedBrowserCompilationHash = action.payload;
+  });
+};
 
-let previouslyCreatedNodes = new Map()
+let previouslyCreatedNodes = new Map();
 
 exports.sourceNodes = ({
   actions,
@@ -27,13 +27,17 @@ exports.sourceNodes = ({
   reporter,
 }) => {
   if (webhookBody && webhookBody.runNumber) {
-    runNumber = webhookBody.runNumber
-    isFirstRun = runNumber === 1
+    runNumber = webhookBody.runNumber;
+    isFirstRun = runNumber === 1;
+  } else {
+    runNumber += paramLessSourceRefreshes;
+    isFirstRun = runNumber === 1;
+    paramLessSourceRefreshes = 1;
   }
 
-  reporter.info(`Using test setup #${runNumber}`)
+  reporter.info(`Using test setup #${runNumber}`);
 
-  const currentlyCreatedNodes = new Map()
+  const currentlyCreatedNodes = new Map();
 
   function createNodeHelper(type, nodePartial) {
     const node = {
@@ -42,57 +46,57 @@ exports.sourceNodes = ({
         type,
         contentDigest: createContentDigest(nodePartial),
       },
-    }
-    actions.createNode(node)
-    currentlyCreatedNodes.set(node.id, node)
+    };
+    actions.createNode(node);
+    currentlyCreatedNodes.set(node.id, node);
   }
 
   // used to create pages and queried by them
   createNodeHelper(`DepPageQuery`, {
     id: `page-query-stable`,
     label: `Stable (always created)`,
-  })
+  });
 
   createNodeHelper(`DepPageQuery`, {
     id: `page-query-changing-but-not-invalidating-html`,
     label: `Stable (always created)`,
     buildRun: runNumber, // important for test setup - this will invalidate page queries, but shouldn't invalidate html (if it's not queried)
-  })
+  });
 
   createNodeHelper(`DepPageQuery`, {
     id: `page-query-changing-data-but-not-id`,
     label: `This is${isFirstRun ? `` : ` not`} a first run`, // this will be queried - we want to invalidate html here
-  })
+  });
 
   createNodeHelper(`DepPageQuery`, {
     id: `page-query-dynamic-${runNumber}`, // this should cause different page path
     label: `This is run number {$runNumber}`,
-  })
+  });
 
   // used by static queries
   createNodeHelper(`DepStaticQuery`, {
     id: `static-query-stable`,
     label: `Stable (always created)`,
-  })
+  });
 
   createNodeHelper(`DepStaticQuery`, {
     id: `static-query-changing-but-not-invalidating-html`,
     label: `Stable (always created)`,
     buildRun: runNumber, // important for test setup - this will invalidate static query, but shouldn't invalidate html (if it's not queried)
-  })
+  });
 
   createNodeHelper(`DepStaticQuery`, {
     id: `static-query-changing-data-but-not-id`,
     label: `This is${isFirstRun ? `` : ` not`} a first run`, // this will be queried - we want to invalidate html here
-  })
+  });
 
   for (const prevNode of previouslyCreatedNodes.values()) {
     if (!currentlyCreatedNodes.has(prevNode.id)) {
-      actions.deleteNode({ node: prevNode })
+      actions.deleteNode({ node: prevNode });
     }
   }
-  previouslyCreatedNodes = currentlyCreatedNodes
-}
+  previouslyCreatedNodes = currentlyCreatedNodes;
+};
 
 exports.createPages = async ({ actions, graphql }) => {
   // testing if expected html/page-data files exist OR don't exist (if stale artifacts are removed)
@@ -103,18 +107,18 @@ exports.createPages = async ({ actions, graphql }) => {
       context: {
         dummyId,
       },
-    })
+    });
   }
 
   // stable page that always gets created
-  createPageHelper(`stable`)
+  createPageHelper(`stable`);
 
   if (isFirstRun) {
     // page exists only in first run
-    createPageHelper(`only-in-first`)
+    createPageHelper(`only-in-first`);
   } else {
     // page exists in any run other than first
-    createPageHelper(`only-not-in-first`)
+    createPageHelper(`only-not-in-first`);
   }
 
   const { data } = await graphql(`
@@ -125,7 +129,7 @@ exports.createPages = async ({ actions, graphql }) => {
         }
       }
     }
-  `)
+  `);
 
   for (const depPageQueryNode of data.allDepPageQuery.nodes) {
     actions.createPage({
@@ -134,19 +138,19 @@ exports.createPages = async ({ actions, graphql }) => {
       context: {
         id: depPageQueryNode.id,
       },
-    })
+    });
   }
-}
+};
 
 exports.onPreBuild = () => {
-  console.log(`[test] onPreBuild`)
-  changedSsrCompilationHash = `not-changed`
-  changedBrowserCompilationHash = `not-changed`
-}
+  console.log(`[test] onPreBuild`);
+  changedSsrCompilationHash = `not-changed`;
+  changedBrowserCompilationHash = `not-changed`;
+};
 
-let counter = 1
+let counter = 1;
 exports.onPostBuild = async ({ graphql }) => {
-  console.log(`[test] onPostBuild`)
+  console.log(`[test] onPostBuild`);
   const { data } = await graphql(`
     {
       allSitePage(filter: { path: { ne: "/dev-404-page/" } }) {
@@ -155,7 +159,7 @@ exports.onPostBuild = async ({ graphql }) => {
         }
       }
     }
-  `)
+  `);
 
   fs.writeJSONSync(
     path.join(
@@ -164,9 +168,9 @@ exports.onPostBuild = async ({ graphql }) => {
       `build-manifest-for-test-${counter++}.json`
     ),
     {
-      allPages: data.allSitePage.nodes.map(node => node.path),
+      allPages: data.allSitePage.nodes.map((node) => node.path),
       changedSsrCompilationHash,
       changedBrowserCompilationHash,
     }
-  )
-}
+  );
+};
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,28 @@
-import PostBodyComponents from "./src/components/post-body-components-ssr"
+const React = require(`react`);
+const { useMoreInfoQuery } = require("./src/hooks/use-more-info-query");
+const Github = require(`./src/components/github`).default;
+const PostBodyComponents = require(`./src/components/post-body-components-ssr`)
+  .default;
 
-export function onRenderBody({ setPostBodyComponents }) {
-  setPostBodyComponents(PostBodyComponents)
+exports.onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents(PostBodyComponents);
+};
+
+exports.wrapRootElement = ({ element }) => (
+  <>
+    <Github />
+    {element}
+  </>
+);
+
+function PageWrapper({ children }) {
+  const data = useMoreInfoQuery();
+  return (
+    <>
+      <h1>{data.site.siteMetadata.moreInfo}</h1>
+      {children}
+    </>
+  );
 }
+
+exports.wrapPageElement = ({ element }) => <PageWrapper>{element}</PageWrapper>;
